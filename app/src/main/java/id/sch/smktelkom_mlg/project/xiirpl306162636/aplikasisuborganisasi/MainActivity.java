72c672754463d516cca74c0c475b0939607ec6f8
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-11-12 13:51:54 +0700

    Menampilkan button siswa

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        findViewById(R.id.buttonSiswa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SiswaActivity.class));
+            }
+        });
     }
 }
