f9dbf283c151186d513aae03069c4b2b84799317
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-11-12 13:50:57 +0700

    Membuat tampilan awal

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
