cdc4404ecfa8053cd06d244da1019961cee1b7db
widiasatria <widiasp@gmail.com>
2016-11-20 23:41:57 +0700

    button daftar

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/RegisterActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/RegisterActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.buttonSiswa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, RegistrasiActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/RegistrasiActivity.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by widiasatria on 11/20/2016.
+ */
+public class RegistrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+
+
+    }
+}
