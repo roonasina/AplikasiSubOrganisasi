e6babcbdfc2bc16cd8f41ffdca8ca2aa243e2930
widiasatria <widiasp@gmail.com>
2016-11-22 18:32:20 +0700

    sambung database

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
@@ -28,13 +28,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-            }
-
-        });
     }
 }
 
