69f8e74ab873dbce12a83bb0721c1ff8bdeff368
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-12-04 22:16:02 +0700

    Login Activity pt 2

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/MainActivity.java
@@ -1,11 +1,9 @@
 package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,29 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.main);
-        Thread t=new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                SharedPreferences  sharedPreferences = getSharedPreferences(Config.FLAG, Context.MODE_PRIVATE);
-
-                if(sharedPreferences.getBoolean(Config.FLAG,true)){
-
-
-                    startActivity(new Intent(MainActivity.this,DefaultIntro.class));
-
-                    SharedPreferences.Editor e=sharedPreferences.edit();
-
-                    e.putBoolean(Config.FLAG,false);
-
-                    e.apply();
-                }
-            }
-        });
-        t.start();
-
-
 
 
         findViewById(R.id.buttonSiswa).setOnClickListener(new View.OnClickListener() {
