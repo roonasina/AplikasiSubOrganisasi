e17569fe7d9905332301647ab14ad20a63662128
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-12-04 22:31:58 +0700

    Admin Activity

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/TampilActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TampilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/UserProfile.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl306162636/aplikasisuborganisasi/UserProfile.java
@@ -1,17 +1,89 @@
 package id.sch.smktelkom_mlg.project.xiirpl306162636.aplikasisuborganisasi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class UserProfile extends AppCompatActivity {
+    public static final String KEY_SUB = "sub";
+    TextView tv;
+    private String sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-    }
+        tv = (TextView) findViewById(R.id.sub);
 
+        findViewById(R.id.imageViewbasket).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("TSBC(Basket)");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageViewmac).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("mac");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageViewmedsan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("memo");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageViewpalwaga).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("palwaga");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageViewpaskibra).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("paskibra");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageViewperpus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("perpus");
+                sub = tv.getText().toString();
+                Intent intent = new Intent(UserProfile.this, TampilActivity.class);
+                intent.putExtra(KEY_SUB, sub);
+                startActivity(intent);
+            }
+        });
+    }
 }
 
